0461195280be4f57916337085825f56304262990
joshuaholloway <jhollow6@asu.edu>
2019-07-02 11:31:09 -0500

    Give the Card class a property stringVal which will hold the value of the card

--- oop/02_deck-of-cards/Program.cs
+++ oop/02_deck-of-cards/Program.cs
@@ -2,11 +2,32 @@
 
 namespace _02_deck_of_cards
 {
+    //=========================================================================
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Card card = new Card();
+
+            // Demonstrating using property
+            card.stringVal = "Ace";                              // set
+            Console.WriteLine($"The value is {card.stringVal}"); // get
+        }
+    }
+    //=========================================================================
+    // Create a class called "Card"
+    class Card
+    {
+        string Value;
+
+        // Give the card class a property "stringVal" which will hold the value of the card
+        // (Ace, 2, 3, ..., 10, Jack, Queen, King)
+        public string stringVal
+        {
+            get { return Value; }
+            set { Value = value; }
         }
     }
 }
