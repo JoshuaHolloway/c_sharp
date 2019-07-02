0055dde31b459f4ab0eb0494f6aa2b6f3743c2ad
joshuaholloway <jhollow6@asu.edu>
2019-07-02 12:18:36 -0500

    Give the Card a property suit which will hold the suit of the card

--- oop/02_deck-of-cards/Program.cs
+++ oop/02_deck-of-cards/Program.cs
@@ -12,8 +12,9 @@ namespace _02_deck_of_cards
             Card card = new Card();
 
             // Demonstrating using property
-            card.stringVal = "Ace";                              // set
-            Console.WriteLine($"The value is {card.stringVal}"); // get
+            card.stringVal = "Ace";      // set                         
+            card.stringSuite = "Heart";  // set
+            Console.WriteLine($"The card is {card.stringVal} of {card.stringSuite}"); // get
         }
     }
     //=========================================================================
@@ -21,6 +22,7 @@ namespace _02_deck_of_cards
     class Card
     {
         string Value;
+        string Suite;
 
         // Give the card class a property "stringVal" which will hold the value of the card
         // (Ace, 2, 3, ..., 10, Jack, Queen, King)
@@ -29,5 +31,14 @@ namespace _02_deck_of_cards
             get { return Value; }
             set { Value = value; }
         }
+
+        // Give the Card a property "suit" which will hold the suit of the card
+        public string stringSuite
+        {  
+            get { return Suite; }
+            set { Suite = value; }
+        }
     }
+
+    
 }
