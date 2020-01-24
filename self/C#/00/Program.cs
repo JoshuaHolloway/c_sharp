6681730a28155e736ff2210ff59e520374dd517e
joshuaholloway <jhollow6@asu.edu>
2020-01-23 19:50:34 -0600

    Changed directory structure

---
+++ self/C#/00/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _00
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
